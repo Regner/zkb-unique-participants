c6b221cc3f6266dd6b14db0641f3b625c93e2aaa
Regner Blok-Andersen <shadowdf@gmail.com>
2016-07-14 12:01:12 +0300

    Processing raw kill mail messages into unique id messages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 
 import os
+import json
 import asyncio
 import logging
 
@@ -14,76 +15,81 @@ logger.setLevel(logging.INFO)
 # App Settings
 NATS_SERVERS = os.environ.get('NATS_SERVERS', 'nats://127.0.0.1:4222')
 
+def find_entity_ids(entity):
+    ids = {}
+    ids['character'] = entity['character']['id'] if 'character' in entity else None
+    ids['corporation'] = entity['corporation']['id'] if 'corporation' in entity else None
+    ids['alliance'] = entity['alliance']['id'] if 'alliance' in entity else None
+
+    return ids
+
+
+def merge_ids(old_ids, new_ids):
+    old_ids['characters'].add(new_ids['character'])
+    old_ids['corporations'].add(new_ids['corporation'])
+    old_ids['alliances'].add(new_ids['alliance'])
+
+    return old_ids
+
+
+def find_all_unique_ids(killmail):
+    ids = {
+        'characters': set(),
+        'corporations': set(),
+        'alliances': set(),
+    }
+
+    new_ids = find_entity_ids(killmail['victim'])
+    ids = merge_ids(ids, new_ids)
+
+    for attacker in killmail['attackers']:
+        new_ids = find_entity_ids(attacker)
+        ids = merge_ids(ids, new_ids)
+
+    ids['characters'].discard(None)
+    ids['corporations'].discard(None)
+    ids['alliances'].discard(None)
+
+    converted_ids = {
+        'characters': list(ids['characters']),
+        'corporations': list(ids['corporations']),
+        'alliances': list(ids['alliances']),
+    }
+
+    return converted_ids
 
 async def run(loop):
     client = Client()
     servers = NATS_SERVERS.split(',')
 
     await client.connect(io_loop=loop, servers=servers)
+    logger.info('Connected to NATS server...')
+
 
     async def message_handler(msg):
-        logger.info('Got message {}'.format(msg))
+        try:
+            data = json.loads(msg.data.decode())
+            killmail = data['killmail']
 
-    sid = await client.subscribe('zkillboard.raw', cb=message_handler)
+            unique_ids = find_all_unique_ids(killmail)
+            logger.info(unique_ids)
+
+            payload = {
+                'ids': unique_ids,
+                'zkb_data': data,
+            }
+
+            count = len(unique_ids['characters']) + len(unique_ids['corporations']) + len(unique_ids['alliances'])
+            logging.info('Found {} unique IDs in killmail ID {}'.format(count, killmail['killID']))
+
+            await client.publish('zkillboard.processed.unique_ids', str.encode(json.dumps(payload)))
+        except Exception as e:
+            logger.info('Got exception: {}'.format(e))
+
+    await client.subscribe('zkillboard.raw', cb=message_handler)
 
 
 if __name__ == '__main__':
   loop = asyncio.get_event_loop()
   loop.run_until_complete(run(loop))
   loop.run_forever()
-
-
-
-
-# def find_entity_ids(entity):
-#     ids = {}
-#     ids['character'] = entity['character']['id'] if character in entity
-#     ids['corporation'] = entity['corporation']['id'] if 'corporation' in entity
-#     ids['alliance'] = entity['alliance']['id'] if 'alliance' in entity
-
-#     return ids
-
-
-# def merge_ids(old_ids, new_ids):
-#     if 'character' in new_ids:
-#         old_ids['characters'].add(character_id)
-
-#     if 'corporation' in new_ids:
-#         old_ids['corporations'].add(corporation_id)
-
-#     if 'alliance' in new_ids:
-#         old_ids['alliances'].add(alliance_id)
-
-
-# def find_all_unique_ids(killmail):
-#     ids = {
-#         'characters': set(),
-#         'corporations': set(),
-#         'alliances': set(),
-#     }
-
-#     new_ids = find_entity_ids(killmail['victim'])
-#     ids = merge_ids(ids, new_ids)
-
-#     for attacker in killmail['attackers']:
-#         new_ids = find_entity_ids(attacker)
-#         ids = merge_ids(ids, new_ids)
-
-#     return ids
-
-
-# @app.route('/', methods=['POST'])
-# def message():
-#     message = request.json['message']
-
-#     killmail = json.loads(base64.b64decode(str(message['message']['data'])))
-#     unique_ids = find_unique_ids(killmail)
-
-#     PS_TOPIC.publish(json.dumps({
-#         'ids': unique_ids,
-#         'killmail': killmail,
-#     })
-    
-#     return ('', 204)
-
-
